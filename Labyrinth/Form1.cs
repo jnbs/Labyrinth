9b5b673134eca6a1be51dfae0a41f6179977c339
Kathrine Tso <turquoisstar@yahoo.com>
2017-10-28 16:32:20 -0400

    Archery kinda works now. Arrows are buggy.

--- Labyrinth/Form1.cs
+++ Labyrinth/Form1.cs
@@ -12,13 +12,54 @@ namespace Labyrinth
 {
     public partial class Form1 : Form
     {
-
-        int spriteX = 0;
-        int spriteY = 0;
+        private System.Windows.Forms.Timer timer1;
+        int[][][] map = new int[16][][];
+        PictureBox background = new System.Windows.Forms.PictureBox();
+        PictureBox rangerBox = new PictureBox();
+        PictureBox wumpusBox = new PictureBox();
+        PictureBox arrowBox = new PictureBox();
+        PictureBox hitBox = new PictureBox();
 
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < map.Length; i++)
+            {
+                map[i] = new int[16][];
+                for (int j = 0; j < map.Length; j++)
+                {
+                    map[i][j] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+                    //occupied by, sight, smell, breeze, wumpus, pit, torch, bonfire
+                }
+                //PictureBox newTile = new PictureBox();
+                //CreateBitmapAtRuntime();
+                initializeMap();
+            }
+        }
+
+        public void initializeMap()
+        {
+            Point rangerInitial = new Point();
+            rangerInitial.X = 50;
+            rangerInitial.Y = 0;
+            rangerBox.Size = new Size(25, 25);
+            this.Controls.Add(rangerBox);
+            rangerBox.Image = Image.FromFile("C:\\Users\\Kathrine Tso\\Source\\Repos\\Labyrinth\\Labyrinth\\Resources\\rangerSprite.bmp");
+            rangerBox.Location = rangerInitial;
+
+            Point wumpusInitial = new Point();
+            wumpusInitial.X = 150;
+            wumpusInitial.Y = 100;
+            wumpusBox.Size = new Size(25, 25);
+            this.Controls.Add(wumpusBox);
+            wumpusBox.Image = Image.FromFile("C:\\Users\\Kathrine Tso\\Source\\Repos\\Labyrinth\\Labyrinth\\Resources\\wumpusSprite.bmp");
+            wumpusBox.Location = wumpusInitial;
+
+            background.Image = Image.FromFile("C:\\Users\\Kathrine Tso\\Source\\Repos\\Labyrinth\\Labyrinth\\Resources\\background.bmp");
+            background.Size = new Size(400, 400);
+            background.Location = new Point(50, 0);
+            this.Controls.Add(background);
+            background.SendToBack();
         }
 
         private void importButton_Click(object sender, EventArgs e)
@@ -38,68 +79,111 @@ namespace Labyrinth
 
         private void button1_Click(object sender, EventArgs e)
         {
-            moveRight();
+            moveRight(rangerBox);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            moveLeft();
+            moveLeft(rangerBox);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            moveUp();
+            moveUp(rangerBox);
         }
 
         private void button4_Click_1(object sender, EventArgs e)
         {
-            moveDown();
+            moveDown(rangerBox);
         }
 
-        private void moveLeft()
+        private void shootButton_Click(object sender, EventArgs e)
         {
-            if (spriteX > 0)
+            arrowBox.Size = new Size(25, 25);
+            this.Controls.Add(arrowBox);
+            arrowBox.Image = Image.FromFile("C:\\Users\\Kathrine Tso\\Source\\Repos\\Labyrinth\\Labyrinth\\Resources\\arrowSprite.bmp");
+            arrowBox.Location = rangerBox.Location;
+            arrowBox.BringToFront();
+            InitTimer();
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 300; // in miliseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveRight(arrowBox);
+            checkCollision(arrowBox, wumpusBox);
+            checkRightWall(arrowBox);
+        }
+
+        private void moveLeft(PictureBox sprite)
+        {
+            Point currentLocation = sprite.Location;
+            if (currentLocation.X > 50)
             {
-                Point currentLocation = sprite1.Location;
                 currentLocation.X -= 25;
-                sprite1.Location = currentLocation;
-                spriteX--;
+                sprite.Location = currentLocation;
             }
         }
 
-        private void moveRight()
+        private void moveRight(PictureBox sprite)
         {
-            if (spriteX < 15)
+            Point currentLocation = sprite.Location;
+            if (currentLocation.X < 425)
             {
-                Point currentLocation = sprite1.Location;
                 currentLocation.X += 25;
-                sprite1.Location = currentLocation;
-                spriteX++;
+                sprite.Location = currentLocation;
             }
         }
 
-        private void moveUp()
+        private void moveUp(PictureBox sprite)
         {
-            if (spriteY > 0)
+            Point currentLocation = sprite.Location;
+            if (currentLocation.Y > 0)
             {
-                Point currentLocation = sprite1.Location;
                 currentLocation.Y -= 25;
-                sprite1.Location = currentLocation;
-                spriteY--;
+                sprite.Location = currentLocation;
             }
         }
 
-        private void moveDown()
+        private void moveDown(PictureBox sprite)
         {
-            if (spriteY < 15)
+            Point currentLocation = sprite.Location;
+            if (currentLocation.Y < 375)
             {
-                Point currentLocation = sprite1.Location;
                 currentLocation.Y += 25;
-                sprite1.Location = currentLocation;
-                spriteY++;
+                sprite.Location = currentLocation;
             }
         }
 
+        private void checkCollision(PictureBox sprite1, PictureBox sprite2)
+        {
+            if (sprite1.Location == sprite2.Location)
+            {
+                timer1.Stop();
+                timer1.Dispose();
+                hitBox.Size = new Size(25, 25);
+                this.Controls.Add(hitBox);
+                hitBox.Image = Image.FromFile("C:\\Users\\Kathrine Tso\\Source\\Repos\\Labyrinth\\Labyrinth\\Resources\\hitSprite.bmp");
+                hitBox.Location = sprite1.Location;
+                sprite1.Dispose();
+                sprite2.Dispose();
+                hitBox.BringToFront();
+            }
+        }
 
+        private void checkRightWall(PictureBox sprite)
+        {
+            if (sprite.Location.X >= 425)
+            {
+                sprite.Dispose();
+            }
+        }
     }
 }
--- Labyrinth/Properties/Resources.Designer.cs
+++ Labyrinth/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Labyrinth.Properties
-{
-
-
+namespace Labyrinth.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,141 @@ namespace Labyrinth.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Labyrinth.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap arrowSprite {
+            get {
+                object obj = ResourceManager.GetObject("arrowSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap blankTile {
+            get {
+                object obj = ResourceManager.GetObject("blankTile", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bonfireSprite {
+            get {
+                object obj = ResourceManager.GetObject("bonfireSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap goldSprite {
+            get {
+                object obj = ResourceManager.GetObject("goldSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hitSprite {
+            get {
+                object obj = ResourceManager.GetObject("hitSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pitSprite {
+            get {
+                object obj = ResourceManager.GetObject("pitSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rangerSprite {
+            get {
+                object obj = ResourceManager.GetObject("rangerSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap torchSprite {
+            get {
+                object obj = ResourceManager.GetObject("torchSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap wumpusSprite {
+            get {
+                object obj = ResourceManager.GetObject("wumpusSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
